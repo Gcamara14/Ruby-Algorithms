7745caf6d12b4068dabddbe007af0f2a5fe9e974
Giovani Camara <Gcamara14@gmail.com>
2016-10-23 17:00:17 -0700

    adds merge sort

---
+++ mergesort.rb
@@ -0,0 +1,25 @@
+def merge_sort(list)
+  if list.length <= 1
+     list
+  else
+    mid = (list.length / 2).floor
+    left = merge_sort(list[0..mid - 1])
+    right = merge_sort(list[mid..list.length])
+    merge(left, right)
+  end
+end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
+
+arr= [123,123,212,1,24,5,2,123,213,1,2,4,12,4,2,31,1,5]
+p merge_sort(arr)
--- quicksort.rb
+++ quicksort.rb
@@ -4,22 +4,128 @@
 #pivot
 #right
 
+#
+# class Array
+#   def quicksort
+#     return [] if empty?
+#
+#     # [ 34, 2,1,5,3]
+#
+#
+#     pivot = delete_at(rand(size))
+#     left, right =  partition(&pivot.method(:>))
+#
+#     return *left.quicksort, pivot, *right.quicksort
+#   end
+# end
+#
+#
+# a = [ 34, 2,1,5,3]
+#
+# p a.quicksort
 
+
+
+
+# class Array
+#   def quicksort
+#     return [] if empty?
+#
+#     #[4,2,6,24,7,134,6,34]
+#
+#     #[4,2,6,24, ,134,6,34]
+#
+#     #7 = pivot
+#
+#
+#     #----
+#
+#     #left [4,2,6,24]
+#     #right [134,6,34]
+#     pivot = delete_at(rand(size))
+#
+#     left, right = partition(&pivot.method(:>))
+#
+#     return *left.quicksort, pivot, *right.quicksort
+#
+#   end
+# end
+#
+# arr = [4,2,6,24,7,134,6,34]
+#
+# p arr.quicksort
+#
+
+# class Array
+#   def quicksort
+#     return [] if empty?
+#
+#     #[45,345,2,3,4,656,3,2,51,1,4,6]
+#
+#     #656 = pivot
+#
+#     #left = [45,345,2,3,4,]
+#     #right = [,3,2,51,1,4,6]]
+#
+#     pivot = delete_at(rand(size))
+#
+#     left, right = partition(&pivot.method(:>))
+#
+#     return *left.quicksort, pivot, *right.quicksort
+#
+#   end
+# end
+#
+#
+# arr = [45,345,2,3,4,656,3,2,51,1,4,6]
+#
+# p arr.quicksort
+#
+#
 class Array
   def quicksort
     return [] if empty?
 
-    # [ 34, 2,1,5,3]
-
+    #[23,5,45,2,3434,56,34,453,1,2,43,2]
+    #
+    #56 = pivot
+    #left =[23,5,45,2,3434,]
+    #right = [34,453,1,2,43,2]
 
     pivot = delete_at(rand(size))
-    left, right =  partition(&pivot.method(:>))
+
+    left, right = partition(&pivot.method(:>))
 
     return *left.quicksort, pivot, *right.quicksort
+
   end
 end
 
+arr = [23,5,45,2,3434,56,34,453,1,2,43,2]
 
-a = [ 34, 2,1,5,3]
+p arr.quicksort
 
-p a.quicksort
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
